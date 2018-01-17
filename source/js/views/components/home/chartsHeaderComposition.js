bfaa7bb79d45028e7d3fb28f5f0628f8cb048688
dev <alexandr.oliinyk@gmail.com>
2018-01-17 17:17:51 +0200

    chore(charts/charts/chartsHeaderComposition) add button || pdf-creator

--- source/js/views/components/home/chartsHeaderComposition.js
+++ source/js/views/components/home/chartsHeaderComposition.js
@@ -1,12 +1,46 @@
-import React, { Component, createElement } from "react";
+import React, { Component } from "react";
+import jspdf from "jspdf";
+import html2canvas from "html2canvas";
 
 export default class ChartsHeaderComposition extends Component {
+
+	// eslint-disable-next-line class-methods-use-this
+	printDocument() {
+		const input = window.document.getElementById("main");
+		html2canvas(input).then(canvas => {
+			const imgData = canvas.toDataURL("image/JPEG");
+			// eslint-disable-next-line new-cap
+			const pdf = new jspdf('', 'mm', [canvas.width, canvas.height]);
+			pdf.addImage(imgData, "JPEG",10,10,canvas.width, canvas.height);
+			pdf.save("charts.pdf");
+		});
+	}
+
 	render() {
-		return createElement(
-			"div",
-			{
-				className: "home-header"
-			});
+		return (
+			<div className="home-header">
+				<div className="left">
+					<div className="month">
+						<p>Select the month</p>
+						<button>Current period</button>
+					</div>
+
+					<div className="year">
+						<p>Select the year</p>
+						<button>2018</button>
+					</div>
+				</div>
+
+				<div className="center">
+					<button className="all-pdf" onClick={this.printDocument.bind(this)}>Save all to PDF</button>
+				</div>
+
+				<div className="right">
+					<p>Selected Tables</p>
+					<div>5</div>
+					<button className="select-pdf">Save selected to PDF</button>
+				</div>
+			</div>
+		);
 	}
 }
-
